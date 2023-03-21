1711ff002dab7e517a790e1ea3eb5013fb29af95
crosstyan <crosstyan@outlook.com>
2023-03-21 20:35:44 +0800

    cpp: cancel

--- example/basic/main.cpp
+++ example/basic/main.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 
 struct SomeThing {
+  event* ev;
   std::unique_ptr<std::string> content;
   int *counter;
 };
@@ -12,17 +13,22 @@ struct SomeThing {
 int main(void) {
   auto counter = new int;
   auto thing = new SomeThing;
-  auto s = std::make_unique<std::string>("Hello World.");
+  auto s = std::make_unique<std::string>("Hello World!");
   thing->counter = counter;
   thing->content = std::move(s);
+  thing->ev = nullptr;
   auto base = event_base_new();
   auto tv = timeval{1, 0};
   auto say_something = [](int fd, short int events, void *arg){
     auto thing = static_cast<SomeThing*>(arg);
     std::cout << "Say: " << *thing->content << "\t" << "counter: " << *thing->counter << "\n";
     *thing->counter += 1;
+    if (*thing->counter > 5 && thing->ev != nullptr){
+      event_del(thing->ev);
+    }
   };
   auto ev = event_new(base, -1, EV_PERSIST, say_something, static_cast<void*>(thing));
+  thing->ev = ev;
   event_add(ev, &tv);
   event_base_dispatch(base);
   delete thing;
