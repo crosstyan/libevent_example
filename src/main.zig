6a9a98432fe69edc80bac20cbb5940e74aae2de6
crosstyan <crosstyan@outlook.com>
2023-03-21 22:03:54 +0800

    typesafe

--- src/main.zig
+++ src/main.zig
@@ -6,17 +6,23 @@ const SomeThing = struct {
     content: []const u8,
     counter: *u32,
 };
-pub fn say_something(fd: c_int, events: c_short, arg: ?*anyopaque) callconv(.C) void {
+pub fn say_something(fd: c_int, events: c_short, thing: *SomeThing) callconv(.C) void {
     _ = fd;
     _ = events;
     // https://www.reddit.com/r/Zig/comments/1119m6g/what_does_aligncast_do/
-    var cast = @alignCast(@alignOf(*SomeThing), arg.?);
-    var thing = @ptrCast(*SomeThing, cast);
     std.debug.print("Say: \"{s}\"\tcounter: {d}\n", .{ thing.content, thing.counter.* });
     thing.counter.* += 1;
-    if(thing.counter.* > 5 and thing.event != null){
+    if (thing.counter.* > 5 and thing.event != null) {
         _ = c.event_del(thing.event);
     }
+}
+
+// Marking your functions with the C calling convention is crucial when you’re calling Zig from C.
+/// A type safe `event_new`
+pub fn event_new(base: *c.event_base, fd: c_int, events: c_short, comptime T: type, callback: *const fn (c_int, c_short, *T) callconv(.C) void, callback_arg: *T) ?*c.event {
+    var cb = @ptrCast(c.event_callback_fn, callback);
+    var arg = @ptrCast(*anyopaque, callback_arg);
+    return c.event_new(base, fd, events, cb, arg);
 }
 
 pub fn main() !void {
@@ -34,7 +40,7 @@ pub fn main() !void {
         .counter = p,
     };
 
-    var base = c.event_base_new();
+    var base = c.event_base_new().?;
     var tv = c.timeval{
         .tv_sec = 1,
         .tv_usec = 0,
@@ -43,7 +49,7 @@ pub fn main() !void {
     // persistent. This means that event remains pending even when its callback
     // is activated. If you want to make it non-pending from within its
     // callback, you can call event_del() on it.
-    var ev = c.event_new(base, -1, c.EV_PERSIST, &say_something, @ptrCast(*anyopaque, thing));
+    var ev = event_new(base, -1, c.EV_PERSIST, SomeThing, &say_something, thing);
     // you don't actually needs `event_self_cbarg()`
     thing.event = ev;
     _ = c.event_add(ev, &tv);
