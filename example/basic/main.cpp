99d53f2ca070ecd19ff6b26451c42cc0cb9369c2
crosstyan <crosstyan@outlook.com>
2023-03-21 22:44:52 +0800

    template

--- example/basic/main.cpp
+++ example/basic/main.cpp
@@ -10,6 +10,17 @@ struct SomeThing {
   int *counter;
 };
 
+namespace ev {
+  // https://stackoverflow.com/questions/15542530/template-argument-deduction-substitution-failed-when-using-stdfunction-and-st
+  /// Have to provide type manually
+  ///
+  /// https://en.cppreference.com/w/cpp/language/template_argument_deduction
+  template<typename T>
+  event* event_new(event_base * base, evutil_socket_t fd, short events, void (*callback) (evutil_socket_t, short, T*) , T* args){
+    return event_new(base, fd, events, reinterpret_cast<void (*)(evutil_socket_t, short, void*)>(callback), static_cast<void*>(args));
+  }
+}
+
 int main(void) {
   auto counter = new int;
   auto thing = new SomeThing;
@@ -19,16 +30,16 @@ int main(void) {
   thing->ev = nullptr;
   auto base = event_base_new();
   auto tv = timeval{1, 0};
-  auto say_something = [](int fd, short int events, void *arg){
+  auto say_something = [](int fd, short int events, SomeThing *thing){
     const auto max_called = 5;
-    auto thing = static_cast<SomeThing*>(arg);
     std::cout << "Say: " << *thing->content << "\t" << "counter: " << *thing->counter << "\n";
     *thing->counter += 1;
     if (*thing->counter >= max_called && thing->ev != nullptr){
       event_del(thing->ev);
     }
   };
-  auto ev = event_new(base, -1, EV_PERSIST, say_something, static_cast<void*>(thing));
+  // template argument deduction/substitution failed
+  auto ev = ev::event_new<SomeThing>(base, -1, EV_PERSIST, say_something, thing);
   thing->ev = ev;
   event_add(ev, &tv);
   event_base_dispatch(base);
